4e7449a196f8e08ce26eb693e0c6874c95956c69
ariel murphy <arielladders@gmail.com>
2020-07-14 13:29:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,13 +12,13 @@ attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 rooms = [1,2,3,4,5,6,7]
 def assign_rooms(attendees)
   attendeess.each_with_index.map {|attendee, index| "Hello, #{attendee}! You'll be assigned to room #{index+1}!"}
-  end
+end
 
 def printer(attendees)
  batch_badge_creator(attendeess).each do |badge|
   puts badge
  end
  
-def assign_rooms(attendeess).each do |assignment|
+assign_rooms(attendeess).each do |assignment|
   puts assignment
- end
+end
