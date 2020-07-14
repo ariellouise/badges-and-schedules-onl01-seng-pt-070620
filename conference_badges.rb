b5316d6c4630d84cafebb7b03e0c5714c085b9fc
ariel murphy <arielladders@gmail.com>
2020-07-14 13:07:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+badge_maker = ["Edsger","Ada","Charles","Alan","Grace","Linus", "Matz"]
+
+badge_maker.each do |name|
+  puts "Hello, my name is #{name}."
+end
+
+#batch_badge_creator(name)
+  
+
+#badge.each do |name|
+  #puts "Taking out #{apple}"
+#end
