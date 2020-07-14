5267e55bb9fc9353750680247e5e6bf3c3fe4130
ariel murphy <arielladders@gmail.com>
2020-07-14 13:28:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,21 +3,21 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-def batch_badge_creator(speakers)
-  speakers.collect {|speaker| badge_maker(speaker)}
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(attendees)
+  attendees.collect {|attendee| badge_maker(attendee)}
 end
 
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 rooms = [1,2,3,4,5,6,7]
-def assign_rooms(speakers)
-  speakers.each_with_index.map {|speaker, index| "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
+def assign_rooms(attendees)
+  attendeess.each_with_index.map {|attendee, index| "Hello, #{attendee}! You'll be assigned to room #{index+1}!"}
   end
 
-def printer(speakers)
- batch_badge_creator(speakers).each do |badge|
+def printer(attendees)
+ batch_badge_creator(attendeess).each do |badge|
   puts badge
  end
- assign_rooms(speakers).each do |assignment|
+ assign_rooms(attendeess).each do |assignment|
   puts assignment
  end
