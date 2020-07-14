af10da09f09a99f2182d28e2fbb75ecedf866224
ariel murphy <arielladders@gmail.com>
2020-07-14 13:21:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,6 +12,7 @@ def batch_badge_creator(speakers)
   speakers.collect {|speaker| badge_maker(speaker)}
 end
 
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 rooms = [1,2,3,4,5,6,7]
 
 def assign_rooms
